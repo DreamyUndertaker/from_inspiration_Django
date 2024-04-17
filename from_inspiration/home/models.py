412f6bc92ef83945e0693ee516e1a2c0e631032e
DreamyUndertaker <vadiimreg73@gmail.com>
2024-04-17 16:45:41 +0400

    Home page started Added model for Product, made CBVList for products

--- from_inspiration/home/models.py
+++ from_inspiration/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    about = models.TextField()
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    image = models.ImageField(upload_to='media/products/img', blank=True)
+
